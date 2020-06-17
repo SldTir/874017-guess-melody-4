bff549c2a43ae9d5677e239d18c2347323934b77
Sld Tir <pasha.tir1991@mail.ru>
2020-06-17 21:44:13 +0300

    Выполнил задание

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,6 +2,42 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../app/app';
 
+const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      picture: `https://api.adorable.io/avatars/128/1`,
+      artist: `John Snow`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/2`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/3`,
+      artist: `Jim Beam`,
+    }],
+  },
+];
+
 const Settings = {
   ERRORS_COUNT: 3
 };
@@ -9,6 +45,7 @@ it(`Render App`, () => {
   const tree = renderer
     .create(<App
       errorsCount={Settings.ERRORS_COUNT}
+      questions={questions}
     />)
     .toJSON();
   expect(tree).toMatchSnapshot();
